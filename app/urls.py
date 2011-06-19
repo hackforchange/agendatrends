51c085c15705fef98392ffafd2666a68d9562e18
Sam Gammon <sg@samgammon.com>
2011-06-19 08:53:02 -0700

    Last interface commit! I swear

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,11 @@ rules = [
 	HandlerPrefix('agendatrends.handlers.', [
 	
 		Rule('/', name='landing', handler='main.LandingHandler'),
+		Rule('/topics', name='topics', handler='site.topics.Landing'),
+		Rule('/people', name='people', handler='site.people.Landing'),
+		Rule('/trends', name='trends', handler='site.trends.Landing'),
+		Rule('/sources', name='sources', handler='site.sources.Landing'),		
+		
 		Rule('/topic/<path:filters>', name='topic', handler='main.TopicHandler'),
 		Rule('/api/topic/<path:filters>', name='api', handler='api.ApiTopicHandler'),
 		Rule('/api/legislators', name='api', handler='api.ApiLegislatorHandler')

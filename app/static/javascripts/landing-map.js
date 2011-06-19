377f25cbb088bd54ba11b7bc0cae15c31c2b57a1
Mike Glazer <mike.glazer@gmail.com>
2011-06-19 08:45:07 -0700

    Switched to protomaps

---
+++ app/static/javascripts/landing-map.js
@@ -0,0 +1,171 @@
+var mapStyles =
+[
+  {
+    featureType: "administrative",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "landscape",
+    elementType: "all",
+    stylers: [
+      { visibility: "simplified" }
+    ]
+  },{
+    featureType: "road",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "all",
+    elementType: "all",
+    stylers: [
+
+    ]
+  }
+];
+
+function initializeWithCoords(coords) {
+/*
+    var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    var mapType = new google.maps.StyledMapType(
+            mapStyles
+            );
+
+    var options = {
+        zoom: 5,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+    };
+    var map = new google.maps.Map(document.getElementById("map-viewer"), options);
+
+    map.mapTypes.set('myMapStyle', mapType);
+    map.setMapTypeId('myMapStyle');
+*/
+
+    var po = org.polymaps;
+
+    var quantile = pv.Scale.quantile()
+        .quantiles(9)
+        .domain(pv.values(states))
+        .range(0, 8);
+
+    var format = pv.Format.date("%B %e, %Y");
+    
+
+    var map = po.map()
+        .container(document.getElementById('map-viewer').appendChild(po.svg("svg")))
+        .center({ lat: coords.latitude, lon: coords.longitude })
+        .zoomRange([5, 6])
+        .zoom(4)
+        .add(po.interact());
+
+    map.add(po.geoJson()
+            .url("http://polymaps.appspot.com/state/{Z}/{X}/{Y}.json")
+            .on("load", load));
+
+    map.container().setAttribute("class", "Blues");
+
+    function load(e) {
+      for (var i = 0; i < e.features.length; i++) {
+        var feature = e.features[i], d = states[feature.data.id.substring(6)];
+        if (d == undefined) {
+          feature.element.setAttribute("display", "none");
+        } else {
+          feature.element.setAttribute("class", "q" + quantile(d) + "-" + 9);
+          feature.element.appendChild(po.svg("title").appendChild(
+              document.createTextNode(feature.data.properties.name + ": "
+              + format(d).replace(/ [ ]+/, " ")))
+              .parentNode);
+        }
+      }
+    }
+    
+
+
+}
+
+function loadLegislatorsWithCoords(coords) {
+    $.getJSON('/api/legislators', 
+            { 
+              'latitude': coords.latitude,
+              'longitude': coords.longitude 
+            },
+        function(data) {
+            loadLegislators(data.legislators);
+        }
+    );
+}
+
+
+var legislators_g = null;
+function loadLegislators(legislators) {
+    legislators_g = legislators;
+    var ulList = $("<ul></ul>");
+    $("#reps").empty().append(ulList);
+    for ( var i = 0, len = legislators.length; i < len; ++i ) {
+        var legislator = legislators[i].legislator;
+        console.log(legislator);
+        if ( legislator['facebook_id'] !== undefined ) {
+            
+            var anchor = $("<a href=\"" + legislator['website'] + "\"><img src=\"http://graph.facebook.com/" + legislator['facebook_id'] + "/picture\"/></a>" +
+                    "<div class=\"tooltip\"><table>" +
+                    "<tr><th>Name:</th><td>" + legislator['firstname'] + " " + legislator['lastname'] + "</td>" +
+                    "<tr><th>Chamber:</th><td>" + legislator['chamber'].titleize() + "</td></tr>" +
+                    "</table></div>")
+                .tooltip({ position: "center right", relative: true});
+            var listItem = $("<li></li>").append(anchor);
+            ulList.append(listItem);
+
+        }
+
+    }
+
+}
+
+
+
+
+var client = new simplegeo.ContextClient('VsTU8grV92mP6NZMbwtdyQ8mNh7MyUQw');
+
+client.getLocation({enableHighAccuracy: true}, function(err, position) {
+        if (err) {
+            // could not retrieve location
+            console.log("ERROR");
+        } else {
+            console.log(position);
+            initializeWithCoords(position.coords);
+            loadLegislatorsWithCoords(position.coords);
+        }
+
+
+});
+
+
+
+$(document).onload(function() {
+    $("#topic-input").click(function() {
+        $.getJSON('/api/rep
+
+
+
+});
+
+	
+
+			

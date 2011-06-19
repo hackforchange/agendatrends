fef7d4df778d2c973d3c95944fbdaf11c9202376
Mike Glazer <mike.glazer@gmail.com>
2011-06-19 11:44:41 -0700

    Updated tags.py

--- app/agendatrends/mappers/tags.py
+++ app/agendatrends/mappers/tags.py
@@ -2,4 +2,8 @@ from mapreduce import operation as op
 
 
 def CountOccurrences(entity):
-	pass
+    yield op.counters.Increment(entity.topic.key().name() + "|" + entity.discourse.key().name())
+    yield op.counters.Increment(entity.topic.key().name() + "|" + entity.discourse.published.year)
+    yield op.counters.Increment(entity.topic.key().name() + "|" + entity.discourse.published.year + "-" + entity.discourse.published.day)
+    yield op.counters.Increment(entity.topic.key().name() + "|" + entity.discourse.published.year + "-" + entity.discourse.published.day + "-" + entity.discourse.published.hour)
+
